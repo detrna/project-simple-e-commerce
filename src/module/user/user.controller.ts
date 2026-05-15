1f0592008a4ddc39d8820003ec739c199f0515d4
detrna <himselfunderscore@gmail.com>
2026-05-15 16:01:26 +0700

    fix: user's type safety fix

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -4,7 +4,7 @@ import { UserService } from "./user.service";
 export class UserController {
   async getUser(req: Request, res: Response) {
     const service = new UserService();
-    const result = await service.getUser(req.params);
+    const result = await service.getUser(req.params.id as string);
     res.json(result);
   }
   async getAllUser(req: Request, res: Response) {
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -2,9 +2,9 @@ import { UserDTO } from "./User";
 import { UserRepository } from "./user.repository";
 
 export class UserService {
-  async getUser(params: any): Promise<UserDTO | null> {
+  async getUser(id: string): Promise<UserDTO | null> {
     const repo = new UserRepository();
-    const user = await repo.getUser(params.id);
+    const user = await repo.getUser(id);
     if (!user) {
       throw new Error("User not found");
     }
@@ -12,18 +12,14 @@ export class UserService {
   }
 
   async getAllUser(): Promise<UserDTO[]> {
-    try {
-
       const repo = new UserRepository();
       const users = await repo.getAllUser();
       
       if(users.length === 0) {
-        throw new Error("error");
+        throw new Error("No users found");
       }
       
       return users.map(user => user.toSafeObject());
-    } catch(e){
-      throw new Error("query error")
-    }
+    
   }
 }
