46f51e45652f4d50ca4cb274aa7ef9e4eb2dad1a
nocturnal <rahmat19hadiwijaya@gmail.com>
2026-05-25 17:24:33 +0700

    store service

--- src/module/store/store.service.ts
+++ src/module/store/store.service.ts
@@ -1,8 +1,26 @@
 
-import { Store } from "./Store";
+import { Store, StoreDTO } from "./Store";
 import { StoreRepository } from "./store.repository";
 
 export class StoreService {
+    async getStore(id: string): Promise<StoreDTO | null> {
+        const repo = new StoreRepository();
+        const store = await repo.getStore(id);
+        if (!store) {
+            throw new Error("Store not found");
+        }
+        return store;
+    }
+    async getAllStore(): Promise<StoreDTO[]> {
+        const repo = new StoreRepository();
+        const stores = await repo.getAllStore();
+
+        if (Store.length === 0) {
+            throw new Error("No stores found");
+        }
+
+        return stores.map(store => store)
+    }
     async getByUid(uid: string): Promise<Store> {
         try {
             const repo = new StoreRepository();
